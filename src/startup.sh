fee7487d2624fe014429642e59068aba8a4a664a
BryanZhang <bryan.zhang@hairobotics.com>
2022-08-29 20:19:37 +0800

    change use for hairobotics

--- src/startup.sh
+++ src/startup.sh
@@ -5,11 +5,11 @@ if ! grep -q ${SSH_USERNAME} /etc/group &> /dev/null; then
 fi
 
 if ! id "${SSH_USERNAME}" &> /dev/null; then
-	adduser --home /opt/home --ingroup ${SSH_USERNAME} ${SSH_USERNAME}
+	adduser --home /home/kubot --ingroup ${SSH_USERNAME} ${SSH_USERNAME}
 fi
 
 echo -e "${SSH_PASSWORD}\n${SSH_PASSWORD}" | passwd ${SSH_USERNAME}
-chown -R ${SSH_USERNAME} /opt/home
+chown -R ${SSH_USERNAME} /home/kubot
 
 service ssh start
 
